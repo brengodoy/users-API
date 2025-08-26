940d0928337d7598ef01282213d68b3f889af642
brengodoy <dia.godoybrenda@gmail.com>
2025-08-26 09:53:44 +0200

    Create user repository

--- application/create_user.py
+++ application/create_user.py
@@ -1,10 +1,11 @@
 from domain.user import User
 from email_validator import validate_email, EmailNotValidError
+from infrastructure.user_repository import UserRepositorySQLite
 
-def create_user(email : str, password : str, repo) -> User:
+def create_user(email : str, password : str, repo : UserRepositorySQLite) -> User:
     valid_email = validate_email_address(email)
-    hash = hash_password(password)
-    user = User(valid_email,hash)
+    password_hash = hash_password(password)
+    user = User(valid_email, password_hash)
     repo.save(user)
     return user
 
---
+++ infrastructure/user_repository.py
@@ -0,0 +1,35 @@
+import sqlite3
+from domain.user import User
+
+class UserRepositorySQLite():
+    def __init__(self, db_path = "users.db"):
+        self.db_path = db_path
+        self.create_table()
+    
+    def create_table(self):
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            sql = """CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                email TEXT UNIQUE,
+                password TEXT)"""
+            cursor.execute(sql)
+            conn.commit()
+    
+    def save(self, user):
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            sql = """INSERT INTO users (email,password) VALUES (?,?)"""
+            cursor.execute(sql, (user.email, user.password_hash))
+            conn.commit()
+            user.id = cursor.lastrowid
+    
+    def find_by_email(self,email):
+        with sqlite3.connect(self.db_path) as conn:
+            cursor = conn.cursor()
+            sql = """SELECT * FROM users WHERE email = ?"""
+            cursor.execute(sql,(email,))
+            row = cursor.fetchone()
+            if row:
+                return User(user_id = row[0],email = row[1], password_hash = row[2])
+            return None
